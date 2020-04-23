a8c9e026dd42f4578725a2d777a133e35bb7e5cd
RomanAstra <roman_muratov_astu@mail.ru>
2020-04-23 19:40:22 +0300

    add Students

---
+++ Assets/BehavioralPatterns/Iterator/Students.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+
+namespace Iterator
+{
+    public sealed class Students
+    {
+        private User[] _users;
+
+        public User[] GetUserForFemale()
+        {
+            return _users.Where(user => user.Gender == Gender.Male).ToArray();
+        }
+    }
+
+    public class User
+    {
+        public Gender Gender;
+    }
+
+    public enum Gender
+    {
+        None   = 0,
+        Male   = 1,
+        Female = 2
+    }
+}
