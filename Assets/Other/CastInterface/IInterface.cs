d92f0428294de5850e56571b844e6b8ed93ba786
RomanAstra <roman_muratov_astu@mail.ru>
2020-07-13 13:08:50 +0300

    Add CastInterface

---
+++ Assets/Other/CastInterface/IInterface.cs
@@ -0,0 +1,39 @@
+﻿namespace Other.CastInterface
+{
+    public interface IInterface
+    {
+        void Do(IAction action);
+    }
+
+    public interface IAction { }
+
+    public sealed class Move : IAction { }
+    
+    public sealed class Rotation : IAction { }
+
+    public abstract class DoAction<T> : IInterface where T : IAction
+    {
+        public void Do(IAction action)
+        {
+            Do((T) action);
+        }
+
+        protected abstract void Do(T action);
+    }
+
+    public sealed class MoveExecutor : DoAction<Move>
+    {
+        protected override void Do(Move action)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public sealed class RotationExecutor : DoAction<Rotation>
+    {
+        protected override void Do(Rotation action)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
